646f5239a8ad923a14c6e7825e1e29d0367b3732
Ben Chan <ben@chanhq.com>
2019-11-16 20:17:49 +0000

    Filter search implemented

--- src/App.js
+++ src/App.js
@@ -22,10 +22,14 @@ class App extends Component{
 
   // Props Children
   render(){
+
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
+
     return(
       <div className="App">
         <input type='search' placeholder='search monsters' onChange={ e => this.setState({searchField: e.target.value}) } />
-        <CardList monsters={this.state.monsters} />
+        <CardList monsters={filteredMonsters} />
         
       </div>
     );
