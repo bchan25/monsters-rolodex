43de47d887e3dc9cdd0d68c19cee7f146cf7b562
Ben Chan <ben@chanhq.com>
2019-11-16 12:58:28 +0000

    Added a State example

--- src/App.js
+++ src/App.js
@@ -3,24 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component{
-  
+  constructor(){
+    super();
+
+    this.state = {
+      string: 'Hello World'
+    };
+  }
+
+
   render(){
     return(
       <div className="App">
       <header className="App-header">
-        <p>Hello Ben.</p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{this.state.string}</p>
+        <button>Change Text</button>
       </header>
     </div>
     );
