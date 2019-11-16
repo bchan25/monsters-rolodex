a0a0670b43e24a368239e503cfe17b285bb08ed1
Ben Chan <ben@chanhq.com>
2019-11-16 12:56:17 +0000

    Converted to Class Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component{
+  
+  render(){
+    return(
+      <div className="App">
       <header className="App-header">
-        <p>Hello my name is Ben.</p>
+        <p>Hello Ben.</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -21,7 +23,9 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    );
+  }
+
 }
 
 export default App;
